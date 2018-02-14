2e96ba19f3b8ba9a92d415b7f2bc7e904aa09a5c
Taiga <taiga@taiga-VirtualBox>
2018-02-14 11:19:06 -0800

    Edits now also affect stylesheet variable

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@
 apt-get update
 apt-get install build-essential
 apt install cmake
-apt-get install flex bison graphviz
+apt-get install flex
+apt-get install bison
+apt-get install graphviz
 git clone https://github.com/doxygen/doxygen.git
 cd doxygen
 mkdir build
@@ -20,6 +22,10 @@ cd ../..
 
 # Update install directory
 sed -i "s@^DOXY_DIR *=.*@DOXY_DIR=$PWD/Doxyfile@" ./doxy || echo "Doxy not in currently working directory. Run from the install directory where you have doxy."
+
+sed -i "s@^LAYOUT_FILE *=.*@LAYOUT_FILE = $PWD/DoxygenLayout.xml@" ./Doxyfile || echo "Doxyfile not in currently working directory. Run from the install directory where you have doxy."
+
+sed -i "s@^HTML_EXTRA_STYLESHEET *=.*@HTML_EXTRA_STYLESHEET = $PWD/customdoxygen.css@" ./Doxyfile || echo "Doxyfile not in currently working directory. Run from the install directory where you have doxy."
 
 # Add doxy to global variabels
 ln -s $PWD/doxy /usr/bin/doxy || \
